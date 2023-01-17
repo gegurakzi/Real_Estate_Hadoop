f9782d15236e0cbc7939599781aaea46025a8dbf
Malachai <xogmldntmd@naver.com>
2023-01-17 22:19:27 +0900

    Fix: mysql 컨테이너 compose 수정, zookeeper myid 생성 script 수정

--- REAME.md
+++ REAME.md
@@ -5,5 +5,12 @@ sudo docker build -t cluster:node .
 ```
 
 ```
-sudo docker exec -it mysql-container mysql -p
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -d mysql:8.0-debian
+sudo docker exec -it metastore mysql -p
+CREATE DATABASE hive;
+CREATE DATABASE airflow;
+CREATE USER hive;
+CREATE USER airflow;
+GRANT ALL PRIVILEGES ON hive.* to 'hive'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON airflow.* to 'airflow'@'%' WITH GRANT OPTION;
 ```
--- lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
+++ lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo docker exec master01 mkdir /usr/local/lib/apache-zookeeper-3.7.1-bin/data
+sudo docker exec master02 mkdir /usr/local/lib/apache-zookeeper-3.7.1-bin/data
+sudo docker exec slave01 mkdir /usr/local/lib/apache-zookeeper-3.7.1-bin/data
 sudo docker exec master01 echo 1 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
 sudo docker exec master02 echo 2 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
 sudo docker exec slave01 echo 3 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
