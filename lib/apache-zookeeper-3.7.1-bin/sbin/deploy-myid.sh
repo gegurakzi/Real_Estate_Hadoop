95e33e5a563dc754c86ed1bf4a6392e0f1a766f3
Malachai <xogmldntmd@naver.com>
2023-01-17 22:26:41 +0900

    Fix: zookeeper myid 생성 script 수정

--- lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
+++ lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
@@ -3,6 +3,6 @@
 sudo docker exec master01 touch /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
 sudo docker exec master02 touch /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
 sudo docker exec slave01 touch /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
-sudo docker exec master01 sh -c 'echo 1 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
-sudo docker exec master02 sh -c 'echo 2 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
-sudo docker exec slave01 sh -c 'echo 3 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
+sudo docker exec -it master01 sh -c 'echo 1 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
+sudo docker exec -it master02 sh -c 'echo 2 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
+sudo docker exec -it slave01 sh -c 'echo 3 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid'
