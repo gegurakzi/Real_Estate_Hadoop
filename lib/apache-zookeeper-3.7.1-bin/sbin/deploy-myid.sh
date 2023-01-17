21bf5484a74db17637a261b2f2cd31f80e73e042
LeeTaehee <prussian1933@naver.com>
2023-01-17 17:50:30 +0900

    Fix: zookeeper journal node myid 배정 script 수정

--- lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
+++ lib/apache-zookeeper-3.7.1-bin/sbin/deploy-myid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo docker exec master01 echo 1 > $ZOOKEEPER_HOME/data/myid
-sudo docker exec master02 echo 2 > $ZOOKEEPER_HOME/data/myid
-sudo docker exec slave01 echo 3 > $ZOOKEEPER_HOME/data/myid
+sudo docker exec master01 echo 1 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
+sudo docker exec master02 echo 2 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
+sudo docker exec slave01 echo 3 > /usr/local/lib/apache-zookeeper-3.7.1-bin/data/myid
