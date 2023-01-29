b664f546c39ea2a78f5ee886f70647483b9c4407
Malachai <xogmldntmd@naver.com>
2023-01-29 17:52:37 +0900

    Feat: Airflow extraction DAG

--- lib/apache-airflow-2.5.0/dags/malachai/crawl/real_estate_workflow.py
+++ lib/apache-airflow-2.5.0/dags/malachai/crawl/real_estate_workflow.py
@@ -9,18 +9,19 @@ import requests
 
 from subprocess import PIPE, Popen
 
+from airflow.operators.empty import EmptyOperator
+
+
 @dag(
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     tags=["extract"],
 )
-def real_estate_workflow():
-    @task()
+def real_estate_workflow(deal_ymd):
+    @task(multiple_outputs=True)
     def extract():
-        now = pendulum.now().time()
         data_url = "https://datafile.seoul.go.kr/bigfile/iot/sheet/csv/download.do"
-        deal_ymd = "20220130"
         form = {
             "srvType": 5,
             "infId": "OA-21275",
@@ -32,13 +33,20 @@ def real_estate_workflow():
         }
 
         r = requests.post(data_url, data=form)
+
+        string_data = r.content.decode('euc-kr')
+        data = StringIO(string_data)
+        df = pd.read_csv(data, sep=",", header=None)
+
+        return df
+
+    @task()
+    def hdfs_load(df):
         file_name = "DEAL_YMD-" + deal_ymd + "-" + \
                     pendulum.now().date().replace('-', '') + "-" + \
                     pendulum.now().time() + '.csv'
         hdfs_path = "/data/" + "DEAL_YMD-" + deal_ymd
-        string_data = r.content.decode('euc-kr')
-        data = StringIO(string_data)
-        df = pd.read_csv(data, sep=",", header=None)
+
         df.to_csv(file_name, encoding='utf-8')
 
         put = Popen(["hadoop", "fs", "-mkdir", hdfs_path], stdin=PIPE, bufsize=-1)
@@ -47,4 +55,9 @@ def real_estate_workflow():
         put = Popen(["hadoop", "fs", "-put", file_name, hdfs_path], stdin=PIPE, bufsize=-1)
         put.communicate()
 
-real_estate_workflow()
+    extract_op = extract(deal_ymd)
+    hdfs_load_op = hdfs_load(extract_op)
+
+    extract_op >> hdfs_load_op
+
+real_estate_workflow_dag = real_estate_workflow("20220130")
