e7ad71dc9105eed0417e6b62034719b4bb0ccfcc
Malachai <prussian1933@naver.com>
2023-02-28 13:02:27 +0900

    Feat: Airflow Hive extract-load DAG completed

--- lib/apache-airflow-2.5.0/dags/real_estate_workflow.py
+++ lib/apache-airflow-2.5.0/dags/real_estate_workflow.py
@@ -1,111 +1,128 @@
 import pendulum
 
 from airflow import DAG
-from airflow.decorators import dag, task
-from airflow.providers.apache.hive.hooks.hive import *
+from airflow.decorators import task
 from airflow.providers.apache.hive.operators.hive import HiveOperator
 from lib.extract.real_estate_csv import real_estate_csv_to_hdfs
 
 with DAG(
-    dag_id="real_estate_workflow_dag",
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=["extract"],
+        dag_id="real_estate_workflow_dag",
+        schedule=None,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        tags=["extract"],
 ) as dag:
+
+    dealymd = "20220128"
+
+    INTERNAL_TABLE_ID = "trade"
+    EXTERNAL_TABLE_ID = "trade_external_db"
+    def hive_cli_connection_id() -> str:
+        return "hive_cli_real_estate"
+    def create_internal_real_estate_table_operation_hql() -> str:
+        return f"""
+        CREATE TABLE IF NOT EXISTS real_estate.{INTERNAL_TABLE_ID}(
+            seq INT,
+            reg_year INT,
+            gu_code INT,
+            gu_name STRING,
+            dong_code INT,
+            dong_name STRING,
+            jibun_type INT,
+            jibun_name STRING,
+            jibun_primary INT,
+            jibun_secondary INT,
+            building_name STRING,
+            deal_price INT,
+            building_area DOUBLE,
+            area DOUBLE,
+            deal_floor INT,
+            right_type STRING,
+            deal_cancel INT,
+            building_year INT,
+            building_usage STRING,
+            deal_type STRING,
+            deal_relator STRING
+        )
+        PARTITIONED BY ( deal_date INT )
+        STORED AS ORC
+        LOCATION 'hdfs:///user/hive/warehouse';
+        """
+    def load_external_real_estate_table_operation_hql(filepath: str) -> str:
+        return f"""
+        DROP TABLE IF EXISTS real_estate.{EXTERNAL_TABLE_ID};
+        CREATE EXTERNAL TABLE real_estate.{EXTERNAL_TABLE_ID} (
+            seq INT,
+            reg_year INT,
+            gu_code INT,
+            gu_name STRING,
+            dong_code INT,
+            dong_name STRING,
+            jibun_type INT,
+            jibun_name STRING,
+            jibun_primary INT,
+            jibun_secondary INT,
+            building_name STRING,
+            deal_price INT,
+            building_area DOUBLE,
+            area DOUBLE,
+            deal_floor INT,
+            right_type STRING,
+            deal_cancel INT,
+            building_year INT,
+            building_usage STRING,
+            deal_type STRING,
+            deal_relator STRING
+        )
+        ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY ','
+        STORED AS TEXTFILE
+        LOCATION 'hdfs://{filepath}'
+        TBLPROPERTIES ('skip.header.line.count'='1');
+        """
+
+    def load_on_real_estate_table_operation_hql() -> str:
+        return f"""
+        INSERT OVERWRITE TABLE real_estate.{INTERNAL_TABLE_ID} PARTITION ( deal_date={dealymd} )
+        SELECT * FROM real_estate.{EXTERNAL_TABLE_ID};
+        """
+
     @task(task_id="extract_csv_file")
     def extract(deal_ymd: str) -> str:
         return real_estate_csv_to_hdfs(deal_ymd)
 
+
     @task(task_id="extract_filepath_print")
-    def extract_filepath_print(src: str) -> str:
+    def extract_filepath_print(src: str):
         print(src)
+
 
     create_table = HiveOperator(
         task_id="create_table_trade",
-        hql="""
-            CREATE TABLE IF NOT EXISTS real_estate.trade(
-                reg_year INT,
-                gu_code INT,
-                gu_name STRING,
-                dong_code INT,
-                dong_name STRING,
-                jibun_type INT,
-                jibun_name STRING,
-                jibun_primary INT,
-                jibun_secondary INT,
-                building_name STRING,
-                deal_date INT,
-                deal_price INT,
-                building_area DOUBLE,
-                area DOUBLE,
-                deal_floor INT,
-                right_type STRING,
-                deal_cancel INT,
-                building_year INT,
-                building_usage STRING,
-                deal_type STRING,
-                deal_relator STRING
-            )STORED AS ORC
-            LOCATION 'hdfs:///user/hive/warehouse'""",
-        hive_cli_conn_id="hive_cli_real_estate",
+        hql=create_internal_real_estate_table_operation_hql(),
+        hive_cli_conn_id=hive_cli_connection_id(),
         run_as_owner=True,
         dag=dag
     )
 
+    res = extract(dealymd)
 
-    load_on_table = HiveOperator(
+    print_res = extract_filepath_print(res)
+
+    load_external_table = HiveOperator(
         task_id="load_csv_on_table",
-        hql="""
-            DROP TABLE IF EXISTS real_estate.trade_external_db;
-            CREATE EXTERNAL TABLE real_estate.trade_external_db (
-                reg_year INT,
-                gu_code INT,
-                gu_name STRING,
-                dong_code INT,
-                dong_name STRING,
-                jibun_type INT,
-                jibun_name STRING,
-                jibun_primary INT,
-                jibun_secondary INT,
-                building_name STRING,
-                deal_date INT,
-                deal_price INT,
-                building_area DOUBLE,
-                area DOUBLE,
-                deal_floor INT,
-                right_type STRING,
-                deal_cancel INT,
-                building_year INT,
-                building_usage STRING,
-                deal_type STRING,
-                deal_relator STRING
-            )
-            ROW FORMAT DELIMITED
-            FIELDS TERMINATED BY ','
-            STORED AS TEXTFILE
-            LOCATION 'hdfs:///user/airflow/data/DEAL_YMD-20220127'
-            TBLPROPERTIES ('skip.header.line.count'='1');
-            """,
-        hive_cli_conn_id = "hive_cli_real_estate",
-        run_as_owner = True,
-        dag = dag
+        hql=load_external_real_estate_table_operation_hql(str(res)),
+        hive_cli_conn_id=hive_cli_connection_id(),
+        run_as_owner=True,
+        dag=dag
     )
 
-    res1 = extract("20220127")
-    res2 = extract("20220128")
-    res3 = extract("20220129")
-    res4 = extract("20220130")
-    res5 = extract("20220131")
+    load_on_internal_table = HiveOperator(
+        task_id="load_external_data_on_internal_table",
+        hql=load_on_real_estate_table_operation_hql(),
+        hive_cli_conn_id=hive_cli_connection_id(),
+        run_as_owner=True,
+        dag=dag
+    )
 
-    res = [res1, res2, res3, res4, res5]
-
-    print1 = extract_filepath_print(res1)
-    print2 = extract_filepath_print(res2)
-    print3 = extract_filepath_print(res3)
-    print4 = extract_filepath_print(res4)
-    print5 = extract_filepath_print(res5)
-
-
-    [print1, print2, print3, print4, print5] >> create_table
-    create_table >> load_on_table
+    print_res >> create_table >> load_external_table >> load_on_internal_table
